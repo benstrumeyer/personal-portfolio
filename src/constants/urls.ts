2c6f8e59bb5a3917c58618025b6feca3b4150f4b
Benjamin Strumeyer <benjamin.strumeyer@gmail.com>
2025-10-14 20:50:01 -0400

    add resume

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
 // External URLs
 export const EXTERNAL_URLS = {
-  RESUME: 'https://benstrumeyer.github.io/portfolio//Ben_Strumeyer_Resume.pdf',
+  RESUME: '/Ben_Strumeyer_Resume.pdf',
   GITHUB: 'https://github.com/benstrumeyer',
   LINKEDIN: 'https://www.linkedin.com/in/ben-strumeyer-8aa897113',
   INSTAGRAM: 'https://www.instagram.com/ben_strumeyer',
